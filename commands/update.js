2df9000252e4f26924ad7b30aea9c42b50937257
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-10-28 19:44:32 -0700

    early update command

---
+++ commands/update.js
@@ -0,0 +1,19 @@
+const { exec } = require('child_process');
+
+exports.run = (client, message, params, perms) => {
+  if (perms < 5) return message.channel.send("You do not have permission to restart the bot.");
+  message.channel.send("Restarting...")
+  .then(msg => {
+    client.restartMsg.set("msg", {channel: msg.channel.id, message: msg.id});
+  })
+  exec('pm2 restart all', (err, stdout, stderr) => {
+    if (err) console.log(err);
+  });
+};
+
+exports.conf = {
+  name: 'restart',
+  aliases: [],
+  permLevel: 0,
+  usage: '+restart'
+};
