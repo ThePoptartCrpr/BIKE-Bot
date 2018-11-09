01310c2bfb6d01987e2630e32b4ef6c0a8cca6c5
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-11-08 17:28:11 -0800

    Fancified +profile

--- commands/level.js
+++ commands/level.js
@@ -7,50 +7,50 @@ exports.run = (client, message, params, perms) => {
     if (!tempUser) tempUser = message.guild.members.find(val => val.user.username.toLowerCase().indexOf(params[0].toLowerCase()) != -1);
   }
   if (tempUser !== null) user = tempUser.user;
-  
+
   // console.log(user);
   console.log("temp", tempUser);
-  
+
   let points = 0;
   let level = 0;
   let balance = 0;
-  
+
   if (client.points.get(user.id) != undefined) {
     points = client.points.get(user.id).points;
     level = client.points.get(user.id).level;
   }
-  
+
   if (client.balance.get(user.id) != undefined) {
     balance = client.balance.get(user.id).bal;
   }
-  
+
   let streak = client.streaks.get(user.id) || {streak: 0};
   let currStats = client.userStats.get(user.id) || {dailies: 0};
-  
+
   let rep = client.reputation.get(user.id) || {rep: 0, sent: 0};
-  
+
   let currPoint = points;
   let nextlevelpoints = 0;
-  
+
   while (Math.floor(0.1 * Math.sqrt(currPoint)) < (level + 1)) {
     currPoint++;
     nextlevelpoints = currPoint;
   }
-  
+
   let minPoint = points;
   let prevlevelpoints = 0;
-  
+
   while (Math.floor(0.1 * Math.sqrt(minPoint)) > (level - 1)) {
     minPoint--;
     prevlevelpoints = minPoint + 1;
   }
-  
+
   progressNextPoints = nextlevelpoints - prevlevelpoints;
   progressPoints = points - prevlevelpoints;
-  
+
   let pointsLeft = nextlevelpoints - points;
-  
-  message.channel.send({embed: {
+
+  /*message.channel.send({embed: {
     color: 3447003,
     author: {
       name: `${user.username}`,
@@ -82,8 +82,23 @@ exports.run = (client, message, params, perms) => {
       }
     ],
     timestamp: new Date()
-  }})
-  
+  }})*/
+
+  message.channel.send({
+    embed: new client.Discord.MessageEmbed()
+      .setAuthor(user.username, user.avatarURL())
+      .addField("Level", `${level}\n\u200B`, true)
+      .addField("Total XP", points.toLocaleString(), true)
+      .addField("Progress", `XP required for level **${level + 1}**: ${nextlevelpoints.toLocaleString()}\n**Progress**: ${progressPoints.toLocaleString()}/${progressNextPoints.toLocaleString()} (${Math.round(progressPoints/progressNextPoints * 100)}%), ${pointsLeft.toLocaleString()} XP left`, true)
+      .addBlankField()
+      .addField("Reputation", `**Received: ${rep.rep}**\n**Sent**: ${rep.sent}`)
+      .addField("Balance", `${client.emojis.find(emoji => emoji.name === "bikecoin")}${balance.toLocaleString()}`, true)
+      .addField("Stats", `**Dailies claimed**: ${currStats.dailies}\n**Current streak**: ${streak.streak}`, true)
+      .setColor(client.EmbedHelper.colors.blue)
+      .setTimestamp()
+      .setFooter("BIKE Alliance", client.user.avatarURL())
+  })
+
 };
 
 exports.conf = {
