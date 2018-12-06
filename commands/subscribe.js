4d01adac700aa1fe928000e60a67bb92e3271023
ThePoptartCrpr <ThePoptartCrpr@users.noreply.github.com>
2018-12-05 19:37:00 -0800

    fix retard moment #4

--- commands/subscribe.js
+++ commands/subscribe.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, [type], perms) => {
   let subscriber_role = message.guild.roles.find(role => role.id === '519755106996977666');
   if (message.member.roles.find(role => role.id === subscriber_role.id)) return message.channel.send({
-    embed: new client.Discord.RichEmbed()
+    embed: new client.Discord.MessageEmbed()
       .setTitle('You are already subscribed to events!')
       .setColor(client.EmbedHelper.colors.red)
       .setTimestamp()
@@ -10,7 +10,7 @@ exports.run = (client, message, [type], perms) => {
 
   message.member.roles.add(subscriber_role.id);
   message.channel.send({
-    embed: new client.Discord.RichEmbed()
+    embed: new client.Discord.MessageEmbed()
       .setTitle('👌 | You are now subscribed to event notifications.')
       .setDescription('Unsubscribe anytime with **+unsubscribe**.')
       .setColor(client.EmbedHelper.colors.lime)
--- commands/unsubscribe.js
+++ commands/unsubscribe.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, [type], perms) => {
   let subscriber_role = message.guild.roles.find(role => role.id === '519755106996977666');
   if (!message.member.roles.find(role => role.id === subscriber_role.id)) return message.channel.send({
-    embed: new client.Discord.RichEmbed()
+    embed: new client.Discord.MessageEmbed()
       .setTitle('You not subscribed to event notifications!')
       .setDescription('You can subscribe anytime with **+subscribe**.')
       .setColor(client.EmbedHelper.colors.red)
@@ -11,7 +11,7 @@ exports.run = (client, message, [type], perms) => {
 
   message.member.roles.remove(subscriber_role.id);
   message.channel.send({
-    embed: new client.Discord.RichEmbed()
+    embed: new client.Discord.MessageEmbed()
       .setTitle('👌 | You are no longer subscribed to event notifications.')
       .setDescription('You can subscribe again with **+subscribe**.')
       .setColor(client.EmbedHelper.colors.lime)
